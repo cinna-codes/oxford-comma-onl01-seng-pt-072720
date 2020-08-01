e840c04aaa5c052253b8425147ad8ea06bcfb463
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-01 21:31:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,14 @@
 def oxford_comma(array)
-if array.length > 1
+if array.length == 1
+  return array
+elsif array.length == 2
+  last_item = "and " + array.pop
+  array << last_item
+  array.join(" ")
+else array.length > 2
   last_item = "and " + array.pop
   array << last_item
   array.join(", ")
+
 end
 end
